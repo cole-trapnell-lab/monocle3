168b82f69b6e806238c79820726fef03c4db72f0
Hannah A Pliner <hpliner@gmail.com>
2019-05-21 15:01:48 -0700

    add check for too long ncenter in learn graph

--- R/preprocess_cds.R
+++ R/preprocess_cds.R
@@ -1,78 +1,42 @@
-#' Project a cell_data_set object into a lower dimensional PCA (or ISI) space
-#' after normalize the data
+#' Preprocess a cds to prepare for trajectory inference
 #'
+#' @description Most analyses (including trajectory inference, and clustering)
+#' in Monocle3, require various normalization and preprocessing steps.
+#' \code{preprocess_cds} executes and stores these preprocessing steps.
 #'
-#'### Data will still be size_factor_normalized! ###
-#' @description For most analysis (including trajectory inference, clustering)
-#' in Monocle 3, it requires us to to start from a low dimensional PCA space.
-#' \code{preprocess_cds} will be used to first project a cell_data_set object
-#' into a lower dimensional PCA space before we apply clustering with community
-#' detection algorithm or other non-linear dimension reduction method, for
-#' example UMAP, tSNE, etc.  While tSNE is especially suitable for visualizing
-#' clustering results, comparing to UMAP, the global distance in tSNE space is
-#' not meaningful. UMAP can either be used for visualizing clustering result or
-#' as a general non-linear dimension reduction method. SimplePPT, DDRTree and
-#' L1-graph are two complementary trajectory inference method where the first
-#' one is very great at learning a tree structure but the later is general and
-#' can learn any arbitrary graph structure. Both methods can be applied to the
-#' UMAP space.
-#'
-#' @details
-#' In Monocle3, we overhauled the code from Monocle2 so that a standard
-#' Monocle3 workflow works as following:
-#' 1. run \code{preprocess_cds} to project a cell_data_set object into a lower
-#' dimensional PCA space after normalizing the data
-#' 2. run \code{reduce_dimension} to further project the PCA space into much
-#' lower dimension space with non-linear dimension reduction techniques,
-#' including tSNE, UMAP.
-#' 3. run \code{cluster_cells} to partition cells into different graphs based
-#' on a similar approach proposed by Alex Wolf and colleagues. We then
-#' reconstruct the trajectory in each partition with the \code{learn_graph}
-#' function.
-#' 4. run \code{learn_graph} to reconstruct developmental trajectory with
-#' reversed graph embedding algorithms. In monocle 3, we enabled the capability
-#' to learn multiple disjointed trajectory with either tree or loop structure,
-#' etc.
-#'
-#' Prior to reducing the dimensionality of the data, it usually helps to
-#' normalize it so that highly expressed or highly variable genes don't
-#' dominate the computation. \code{reduce_dimension()} automatically transforms
-#' the data in one of several ways depending on the \code{expression_family} of
-#' the cell_data_set object. If the expression_family is \code{"negbinomial"}
-#' or \code{"negbinomial.size"}, the data are variance-stabilized. If the
-#' expression_family is \code{"Tobit"}, the data are adjusted by adding a
-#' pseudocount (of 1 by default) and then log-transformed. If you don't want
-#' any transformation at all, set \code{norm_method} to "none" and
-#' \code{pseudo_count} to 0. This maybe useful for single-cell qPCR data, or
-#' data you've already transformed yourself in some way.
+#' Specifically, depending on the options selected, \code{preprocess_cds} first
+#' normalizes the data by log and size factor to address depth differences, or
+#' by size factor only. Next, \code{preprocess_cds} calculates a lower
+#' dimensional space that will be used as the input for further dimensionality
+#' reduction like tSNE and UMAP.
 #'
 #' @param cds the cell_data_set upon which to perform this operation
-#' @param method the initial dimension method to use, current either PCA or
-#'   LSI. For LSI (latent semantic indexing), it converts the (sparse)
-#'   expression matrix into tf-idf (term-frequency-inverse document frequency
-#'   which increases proportionally to the gene expression value appears in the
-#'   cell and is offset by the frequency of the gene in the entire dataset,
-#'   which helps to adjust for the fact that some gene appear more frequently
-#'   across cell in general.) matrix and then performs SVD to decompose the
-#'   gene expression / cells into certain modules / topics. This method can be
-#'   used to find associated gene modules and cell clusters at the same time.
-#'   It removes noise in the data and thus makes the UMAP result even better.
-#' @param num_dim the dimensionality of the reduced space. Ignored if
-#'   method = "none".
+#' @param method a string specifying the initial dimension method to use,
+#'   currently either PCA or LSI. For LSI (latent semantic indexing), it
+#'   converts the (sparse) expression matrix into tf-idf matrix and then
+#'   performs SVD to decompose the gene expression / cells into certain
+#'   modules / topics. Default is "PCA".
+#' @param num_dim the dimensionality of the reduced space.
 #' @param norm_method Determines how to transform expression values prior to
-#'   reducing dimensionality
-#' @param residual_model_formula_str A model formula specifying the effects to
-#'   subtract from the data before clustering.
-#' @param pseudo_count amount to increase expression values before
-#'   dimensionality reduction. If NULL (default), pseudo_count of 1 is added
-#'   for log normalization and 0 is added for no normalization.
+#'   reducing dimensionality. Options are "log" and "size_only". Default is
+#'   "log".
+#' @param use_genes NULL or a list of gene IDs. If a list of gene IDs, only
+#'   this subset of genes is used for dimensionality reduction. Default is
+#'   NULL.
+#' @param residual_model_formula_str NULL or a string model formula specifying
+#'   any effects to subtract from the data before dimensionality reduction.
+#'   Default is NULL.
+#' @param pseudo_count NULL or the amount to increase expression values before
+#'   normalization and dimensionality reduction. If NULL (default), a
+#'   pseudo_count of 1 is added for log normalization and 0 is added for size
+#'   factor only normalization.
 #' @param scaling When this argument is set to TRUE (default), it will scale
 #'   each gene before running trajectory reconstruction. Relevant for
 #'   method = PCA only.
 #' @param verbose Whether to emit verbose output during dimensionality
 #'   reduction
-#' @param ... additional arguments to pass to the dimensionality reduction
-#'   function
+#' @param ... additional arguments to pass to limma::lmFit if
+#'   residual_model_formula is not NULL
 #' @return an updated cell_data_set object
 #' @export
 preprocess_cds <- function(cds, method = c('PCA', "LSI"),
@@ -84,7 +48,7 @@ preprocess_cds <- function(cds, method = c('PCA', "LSI"),
                            scaling = TRUE,
                            verbose=FALSE,
                            ...) {
-  extra_arguments <- list(...)
+
   assertthat::assert_that(
     tryCatch(expr = ifelse(match.arg(method) == "",TRUE, TRUE),
              error = function(e) FALSE),
@@ -166,9 +130,10 @@ preprocess_cds <- function(cds, method = c('PCA', "LSI"),
 
   if (!is.null(residual_model_formula_str)) {
     if (verbose) message("Removing batch effects")
-    X.model_mat <- Matrix::sparse.model.matrix(stats::as.formula(residual_model_formula_str),
-                                       data = colData(cds),
-                                       drop.unused.levels = TRUE)
+    X.model_mat <- Matrix::sparse.model.matrix(
+      stats::as.formula(residual_model_formula_str),
+      data = colData(cds),
+      drop.unused.levels = TRUE)
 
     fit <- limma::lmFit(Matrix::t(preproc_res), X.model_mat, ...)
     beta <- fit$coefficients[, -1, drop = FALSE]
@@ -250,7 +215,8 @@ tfidf <- function(count_matrix, frequencies=TRUE, log_scale_tf=TRUE,
     tf_idf_counts = tf * idf
     tf_idf_counts
   }, error = function(e) {
-    print("TF*IDF multiplication too large for in-memory, falling back on DelayedArray.")
+    print(paste("TF*IDF multiplication too large for in-memory, falling back",
+                "on DelayedArray."))
     options(DelayedArray.block.size=block_size)
     DelayedArray:::set_verbose_block_processing(TRUE)
 
