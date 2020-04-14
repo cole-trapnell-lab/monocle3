e0518f84ff8bf5f689afee16e7484a6221f71393
Brent Ewing <brent@ness.eirene.local>
2020-04-14 16:16:25 -0700

    learn_graph()/order_cells() fixes

--- R/order_cells.R
+++ R/order_cells.R
@@ -110,14 +110,12 @@ order_cells <- function(cds,
 }
 
 extract_general_graph_ordering <- function(cds,
-                                           root_cell,
+                                           root_pr_nodes,
                                            verbose=T,
                                            reduction_method) {
   Z <- t(reducedDims(cds)[[reduction_method]])
   Y <- cds@principal_graph_aux[[reduction_method]]$dp_mst
   pr_graph <- principal_graph(cds)[[reduction_method]]
-
-  res <- list(subtree = pr_graph, root = root_cell)
 
   parents <- rep(NA, length(igraph::V(pr_graph)))
   states <- rep(NA, length(igraph::V(pr_graph)))
@@ -130,15 +128,11 @@ extract_general_graph_ordering <- function(cds,
   # 1. identify nearest cells to the selected principal node
   # 2. build a cell-wise graph for each Louvain group
   # 3. run the distance function to assign pseudotime for each cell
-  closest_vertex <- find_nearest_vertex(Y[, root_cell, drop = F], Z)
-  closest_vertex_id <- colnames(cds)[closest_vertex]
-
   cell_wise_graph <-
     cds@principal_graph_aux[[reduction_method]]$pr_graph_cell_proj_tree
   cell_wise_distances <- igraph::distances(cell_wise_graph,
-                                           v = closest_vertex_id)
-
-  if (length(closest_vertex_id) > 1){
+                                           v = root_pr_nodes )
+  if (length(root_pr_nodes) > 1){
     node_names <- colnames(cell_wise_distances)
     pseudotimes <- apply(cell_wise_distances, 2, min)
   }else{
