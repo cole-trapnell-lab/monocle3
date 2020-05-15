ae3a4689684565a117d95a809bec62ca9905757b
Combiz Khozoie <31043044+combiz@users.noreply.github.com>
2020-05-15 16:56:27 +0100

    Update cell_data_set.R
    fixed assertion evaluating to false for sparse matrices due to the first condition evaluating false.

--- R/cell_data_set.R
+++ R/cell_data_set.R
@@ -67,7 +67,7 @@ new_cell_data_set <- function(expression_data,
                               cell_metadata = NULL,
                               gene_metadata = NULL) {
 
-  assertthat::assert_that(class(expression_data) == "matrix" ||
+  assertthat::assert_that(class(expression_data) == "matrix" |
                             is_sparse_matrix(expression_data),
                           msg = paste("Argument expression_data must be a",
                                       "matrix - either sparse from the",
