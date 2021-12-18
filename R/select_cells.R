be6d879f95f6cabacd73332cff140ac4131026fe
brgew <bge@uw.edu>
2021-12-18 13:48:40 -0800

    Documentation fixes.

--- R/select_cells.R
+++ R/select_cells.R
@@ -10,15 +10,6 @@
 #'
 #' @return A subset CDS object. If return_list = FALSE, a list of cell names.
 #'
-#' @examples
-#' \dontrun{
-#'   cds <- load_worm_embryo()
-#'   cds <- preprocess_cds(cds)
-#'   cds <- reduce_dimension(cds)
-#'   cds <- cluster_cells(cds)
-#'   cds_choose <- choose_cells(cds, "UMAP")
-#' }
-#'
 #' @export
 #'
 choose_cells <- function(cds,
@@ -156,17 +147,6 @@ choose_cells <- function(cds,
 #'
 #' @return A subset CDS object. If return_list = FALSE, a list of cell and
 #'   graph node names.
-#'
-#' @examples
-#' \dontrun{
-#'   cds <- load_worm_embryo()
-#'   cds <- preprocess_cds(cds, num_dim=50)
-#'   cds <- align_cds(cds, alignment_group = "batch", residual_model_formula_str = "~ bg.300.loading + bg.400.loading + bg.500.1.loading + bg.500.2.loading + bg.r17.loading + bg.b01.loading + bg.b02.loading")
-#'   cds <- reduce_dimension(cds)
-#'   cds <- cluster_cells(cds)
-#'   cds <- learn_graph(cds)
-#'   cds_choose <- choose_graph_segments(cds, "UMAP")
-#' }
 #'
 #' @importFrom plyr "."
 #' @export
