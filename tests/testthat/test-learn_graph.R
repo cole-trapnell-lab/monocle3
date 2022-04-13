c34deb877c9d3f6f629347f191ecf81e7cbad510
brgew <bge@uw.edu>
2022-04-12 20:28:40 -0700

    Travis fixes.

--- tests/testthat/test-learn_graph.R
+++ tests/testthat/test-learn_graph.R
@@ -91,7 +91,7 @@ test_that("learn_graph stays the same", {
   skip_not_travis()
   cds <- learn_graph(cds)
   expect_is(principal_graph(cds)[["UMAP"]], "igraph")
-  expect_equal(length(principal_graph(cds)[["UMAP"]]), 10)
+  expect_equal(length(principal_graph(cds)[["UMAP"]]), 48)
   expect_equal(as.character(principal_graph(cds)[["UMAP"]][[1]]$Y_1[[1]]), "9")
 
   # Force partition
@@ -100,22 +100,22 @@ test_that("learn_graph stays the same", {
   cds@clusters[["UMAP"]]$partitions <- temp
   cds <- learn_graph(cds, use_partition = FALSE)
   expect_is(principal_graph(cds)[["UMAP"]], "igraph")
-  expect_equal(length(principal_graph(cds)[["UMAP"]]), 10)
+  expect_equal(length(principal_graph(cds)[["UMAP"]]), 48)
   expect_equal(as.character(principal_graph(cds)[["UMAP"]][[1]]$Y_1[[1]]), "9")
 
   cds <- learn_graph(cds)
   expect_is(principal_graph(cds)[["UMAP"]], "igraph")
-  expect_equal(length(principal_graph(cds)[["UMAP"]]), 10)
+  expect_equal(length(principal_graph(cds)[["UMAP"]]), 66)
   expect_equal(as.character(principal_graph(cds)[["UMAP"]][[1]]$Y_1[[1]]), "28")
 
   cds <- learn_graph(cds, close_loop = TRUE)
   expect_is(principal_graph(cds)[["UMAP"]], "igraph")
-  expect_equal(length(principal_graph(cds)[["UMAP"]]), 10)
+  expect_equal(length(principal_graph(cds)[["UMAP"]]), 66)
   expect_equal(as.character(principal_graph(cds)[["UMAP"]][[1]]$Y_1[[1]]), "28")
 
   cds <- learn_graph(cds, learn_graph_control = list(prune_graph = FALSE))
   expect_is(principal_graph(cds)[["UMAP"]], "igraph")
-  expect_equal(length(principal_graph(cds)[["UMAP"]]), 10)
+  expect_equal(length(principal_graph(cds)[["UMAP"]]), 144)
   expect_equal(as.character(principal_graph(cds)[["UMAP"]][[1]]$Y_1[[1]]), "10")
 })
 
