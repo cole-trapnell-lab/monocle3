66e46c385962b84ea944914ad58a9196d04316b4
brgew <bge@uw.edu>
2023-07-28 12:00:42 -0700

    Fix preprocess_transform.

--- R/projection.R
+++ R/projection.R
@@ -247,7 +247,7 @@ preprocess_transform <- function(cds, reduction_method=c('PCA', 'LSI'), block_si
     row_sums <- cds@reduce_dim_aux[[reduction_method]][['model']][['row_sums']]
     num_cols <- cds@reduce_dim_aux[[reduction_method]][['model']][['num_cols']]
 
-    FM <- SingleCellExperiment::counts(cds) 
+    mat_counts <- SingleCellExperiment::counts(cds) 
     matrix_control_res <- set_pca_matrix_control(mat=mat_counts, matrix_control=matrix_control)
     FM <- set_matrix_class(mat=mat_counts, matrix_control=matrix_control_res)
     FM <- normalize_expr_data(FM=FM, size_factors=size_factors(cds), norm_method=norm_method, pseudo_count=pseudo_count)
